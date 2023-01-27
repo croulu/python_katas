99ea6ac9dda4ca15e3414bd41273fba26aa73b11
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-27 09:48:23 +0100

    mango ok

--- mango.py
+++ mango.py
@@ -1,2 +1,18 @@
+UNITARY_PRICE = 3
+
+
 def mango(quantity, price):
-    pass
+    return pay_how_much(quantity) * price
+
+
+def offer_how_much(quantity):
+    offer = quantity // 3
+    return offer
+
+
+def pay_how_much(quantity):
+    return quantity - offer_how_much(quantity)
+
+
+# mango(3, 12)
+# offer_how_much(0)
--- mango.test.py
+++ mango.test.py
@@ -1,9 +1,30 @@
 import unittest
 
+from mango import mango, offer_how_much, pay_how_much
+
+DEAL1 = 4
+OFFER_FOR_DEAL1 = 1
+PAY_FOR_DEAL1 = 3
+
+DEAL2 = 9
+OFFER_FOR_DEAL2 = 3
+PAY_FOR_DEAL2 = 6
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    # @unittest.skip("pas fini")
+    def test_mango(self):
+        self.assertEqual(mango(3, 3), 6)
+        self.assertEqual(mango(9, 5), 30)
+
+    def test_offer_how_much(self):
+        self.assertEqual(offer_how_much(DEAL1), OFFER_FOR_DEAL1)
+        self.assertEqual(offer_how_much(DEAL2), OFFER_FOR_DEAL2)
+
+    def test_pay_how_much(self):
+        self.assertEqual(pay_how_much(DEAL1), PAY_FOR_DEAL1)
+        self.assertEqual(pay_how_much(DEAL2), PAY_FOR_DEAL2)
 
 
 if __name__ == '__main__':
