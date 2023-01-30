68dd6ed1059db85b0915703b06f097c8825b009a
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-30 18:46:56 +0100

    people in the bus ok

--- people_in_the_bus.py
+++ people_in_the_bus.py
@@ -1,2 +1,16 @@
 def people_in_the_bus(bus_stops):
-    return 0
+    index = 0
+    sum_final = 0
+
+    while index < len(bus_stops):
+        sum_final += pay_stop(bus_stops[index])
+        index += 1
+
+    return sum_final
+
+
+def pay_stop(in_out):
+    people_in = in_out[0]
+    people_out = in_out[1]
+
+    return people_in - people_out
--- people_in_the_bus.test.py
+++ people_in_the_bus.test.py
@@ -1,9 +1,15 @@
 import unittest
 
-from people_in_the_bus import people_in_the_bus
+from people_in_the_bus import people_in_the_bus, pay_stop
 
+in_out_1 = [10, 0]
+in_out_2 = [3, 5]
 
 class MyTestCase(unittest.TestCase):
+    def test_pay_stop(self):
+        self.assertEqual(pay_stop(in_out_1), 10)
+        self.assertEqual(pay_stop(in_out_2), -2)
+
     def test_people_in_the_bus(self):
         self.assertEqual(people_in_the_bus([[10,0],[3,5],[5,8]]),5)
         self.assertEqual(people_in_the_bus([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]),17)
