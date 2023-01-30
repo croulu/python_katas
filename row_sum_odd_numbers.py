b10505a26b17b6bce2e2adf95a2bb355e38fb1ee
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2023-01-30 18:13:51 +0100

    row_sum_odd_numbers ok

--- row_sum_odd_numbers.py
+++ row_sum_odd_numbers.py
@@ -1,2 +1,34 @@
 def row_sum_odd_numbers(n):
-    pass
+    return sum_elements(create_odds_for_me(n))
+
+
+def say_odd(n):
+    return n % 2 != 0
+
+
+def create_odds_for_me(n):
+    odds = []
+    index_triangle = 1
+    index_numbers = 1
+
+    while index_triangle <= n:
+        while len(odds) < index_triangle:
+            if say_odd(index_numbers):
+                odds.append(index_numbers)
+            index_numbers += 1
+        if index_triangle != n:
+            odds = []
+        index_triangle += 1
+
+    return odds
+
+
+def sum_elements(numbers):
+    index = 0
+    sum = 0
+
+    while index < len(numbers):
+        sum += numbers[index]
+        index += 1
+
+    return sum
--- row_sum_odd_numbers.test.py
+++ row_sum_odd_numbers.test.py
@@ -1,10 +1,30 @@
 import unittest
 
-from row_sum_odd_numbers import row_sum_odd_numbers
+from row_sum_odd_numbers import say_odd, create_odds_for_me, sum_elements, row_sum_odd_numbers
+
+odds_for_2 = [3, 5]
+odds_for_3 = [7, 9, 11]
+odds_for_5 = [21, 23, 25, 27, 29]
 
 
 class MyTestCase(unittest.TestCase):
-    def basic_test_cases(self):
+
+    def test_even_or_odd(self):
+        self.assertEqual(say_odd(1), True)
+        self.assertEqual(say_odd(2), False)
+        self.assertEqual(say_odd(5), True)
+        self.assertEqual(say_odd(10), False)
+
+    def test_odds_for_me_and_previous(self):
+        self.assertEqual(create_odds_for_me(2), odds_for_2)
+        self.assertEqual(create_odds_for_me(3), odds_for_3)
+        self.assertEqual(create_odds_for_me(5), odds_for_5)
+
+    def test_sum_all(self):
+        self.assertEqual(sum_elements(odds_for_2), 8)
+        self.assertEqual(sum_elements(odds_for_3), 27)
+
+    def test_basic_test_cases(self):
         self.assertEqual(row_sum_odd_numbers(1), 1)
         self.assertEqual(row_sum_odd_numbers(2), 8)
         self.assertEqual(row_sum_odd_numbers(13), 2197)
